0ee12cbf0ac8fa919fd43e75f99be86ad852329e
Latefs <latefaalhassan@hotmail.com>
2018-11-26 16:11:26 +0000

    challenge2

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,7 +6,23 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
-
+    	"restaurants": [
+	    	{
+		        "name": "Ask Italian",
+		        "type": "italian",
+		        "rating": "4.5"
+	    	},
+	    	{
+		        "name": "Fridays",
+		        "type": "American",
+		        "rating": "5"
+		    },
+		    {
+		        "name": "Noodles",
+		        "type": "Chinese",
+		        "rating": "3.5"
+		    }
+    	]
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +30,12 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
-
-    }
+	    "resturaunt": {
+			    "name": "Ask Italian",
+			    "type": "italian",
+			    "rating": "4.5",
+			    "location": "union square",
+			    "style": "cosy",
+            }
+            }
     return render(request, 'detail.html', context)
---
+++ task_03/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/latefaalhassan/Development/task_03/task_03/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
